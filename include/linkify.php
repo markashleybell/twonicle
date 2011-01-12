6a87ac435e9d9b47bc12648de38d998dde20c018
Mark Bell <me@markashleybell.com>
2011-01-12 20:25:24 +0000

    Linkify function now links hashtags to Twitter search. Closes #2.

--- include/linkify.php
+++ include/linkify.php
@@ -1,8 +1,15 @@
 <?php
 
+function linkify_callback($matches)
+{
+	return '<a href="http://search.twitter.com/search?q=' . urlencode($matches[1]) . '">' . $matches[1] . '</a>';
+}
+
 function linkify($text)
 {
-	return preg_replace('/(https?:\/\/\S+)/', '<a href="\1">\1</a>', $text);
+	$output = preg_replace('/(https?:\/\/\S+)/', '<a href="\1">\1</a>', $text);
+	$output = preg_replace_callback('/(\#\S+)/', "linkify_callback", $output);
+	return $output;
 
 }
 
