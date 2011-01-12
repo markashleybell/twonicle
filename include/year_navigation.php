053758558683a0199ccb4ee2964a530043724e63
Mark Bell <me@markashleybell.com>
2011-01-12 18:24:39 +0000

    Added leading zero to month nav urls

--- include/year_navigation.php
+++ include/year_navigation.php
@@ -6,7 +6,7 @@
     $result = $db->getYearNavigation($year, $showall);
     
     foreach ($result as $month) {
-        echo '<li><a href="' . $config['app_base_path'] . $month->year . '/' . $month->number . '"><span>' . $month->name . ' ' . $month->year . ' (' . $month->count . ')</span></a></li>';
+        echo '<li><a href="' . $config['app_base_path'] . $month->year . '/' . str_pad($month->number, 2, "0", STR_PAD_LEFT) . '"><span>' . $month->name . ' ' . $month->year . ' (' . $month->count . ')</span></a></li>';
     }
     ?>
 </ul>
