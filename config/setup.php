58239250bd54c058f441fb9dc555754358c1900e
Mark Bell <me@markashleybell.com>
2011-01-16 09:46:06 +0000

    Minor base path change to setup.php

--- config/setup.php
+++ config/setup.php
@@ -71,7 +71,7 @@ CONFIG;
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 	<head>
-        <link rel="stylesheet" type="text/css" href="/<?php echo $basepath; ?>css/updater.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo $basepath; ?>css/updater.css" />
         <link rel="shortcut icon" href="/<?php echo $basepath; ?>img/site/favicon.ico" />
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
         <script type="text/javascript">
