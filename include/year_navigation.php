5c50b6ce02318adf467a310ff3f67833abea32b5
Mark Bell <me@markashleybell.com>
2011-01-18 06:16:56 +0000

    Year nav now shows all months on all pages, and highlights current month. Closes #21.

--- include/year_navigation.php
+++ include/year_navigation.php
@@ -1,13 +1,13 @@
 <ul id="year-navigation">
     <?php
-    $navbasepath = ($config['app_base_path'] == '') ? '' : $config['app_base_path'] . '/';
-    $year = (isset($_GET["y"])) ? $_GET["y"] : 0;
-    $showall = ($year == 0) ? true : false;
+    $nav_basepath = ($config['app_base_path'] == '') ? '' : $config['app_base_path'] . '/';
+    $nav_year = (isset($_GET["y"])) ? $_GET["y"] : 0;
+    $nav_month = (isset($_GET["m"])) ? $_GET["m"] : 0;
 
-    $result = $db->getYearNavigation($year, $showall);
+    $result = $db->getYearNavigation($nav_year, true);
     
     foreach ($result as $month) {
-        echo '<li><a href="/' . $navbasepath . $month->year . '/' . str_pad($month->number, 2, "0", STR_PAD_LEFT) . '"><span>' . $month->name . ' ' . $month->year . ' (' . $month->count . ')</span></a></li>';
+        echo '<li' . (($nav_month == $month->number) ? ' class="current-month"' : '') .  '><a href="/' . $nav_basepath . $month->year . '/' . str_pad($month->number, 2, "0", STR_PAD_LEFT) . '"><span>' . $month->name . ' ' . $month->year . ' (' . $month->count . ')</span></a></li>';
     }
     ?>
 </ul>
