e0ce3d2fb667f52557d063c84ecd1255794b9e1a
Mark Bell <me@markashleybell.com>
2011-01-30 09:37:20 +0000

    Link text over 35 chars is truncated. Closes #27.

--- include/linkify.php
+++ include/linkify.php
@@ -1,6 +1,14 @@
 <?php
 
-function linkify_callback($matches) {
+function link_callback($matches) {
+    
+    // '<a href="\1">\1</a>'
+    
+    return '<a href="' . $matches[1] . '">' . ((strlen($matches[1]) > 35) ? substr($matches[1], 0, 32) . '...' : $matches[1]) . '</a>';
+    
+}
+
+function hashtag_callback($matches) {
     
     return '<a href="http://search.twitter.com/search?q=' . urlencode($matches[1]) . '">' . $matches[1] . '</a>';
     
@@ -14,8 +22,8 @@ function username_callback($matches) {
 
 function linkify($text) {
     
-    $output = preg_replace('/(https?:\/\/\S+)/', '<a href="\1">\1</a>', $text);
-    $output = preg_replace_callback('/(\#\S+)/', "linkify_callback", $output);
+    $output = preg_replace_callback('/(https?:\/\/\S+)/', "link_callback", $text);
+    $output = preg_replace_callback('/(\#\S+)/', "hashtag_callback", $output);
     $output = preg_replace_callback('/(@([a-zA-Z0-9_]+))/', "username_callback", $output);
     return $output;
 
