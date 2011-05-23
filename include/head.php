619302b334a76b30dda9c25af8a648a496b68c91
Mark Bell <me@markashleybell.com>
2011-05-23 09:49:23 +0100

    Made search POST url absolute. Closes #41

--- include/head.php
+++ include/head.php
@@ -2,7 +2,7 @@
     <p><a href="/<?php echo $config['app_base_path']; ?>">Home Page</a>
     | <a href="/<?php echo $config['app_base_path']; ?>/picks">Picks</a>
     | <a href="/<?php echo $config['app_base_path']; ?>/picks/text">Picks (Plain Text)</a></p>
-    <form id="search-form" action="search.php">
+    <form id="search-form" action="/<?php echo $config['app_base_path']; ?>/search.php">
         <p><input type="text" name="q" value="<?php echo ((isset($_GET["q"])) ? $_GET["q"] : ''); ?>" /></p>
     </form>
 </div>
