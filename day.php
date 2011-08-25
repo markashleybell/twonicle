ad02b07300b93671b1e5a61f6311192d4d80c3ae
Mark Bell <me@markashleybell.com>
2011-08-25 10:40:13 +0100

    Updated no-cache headers

--- day.php
+++ day.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Cache-Control: private, must-revalidate, max-age=0");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Pragma: no-cache");
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 
